9d5bfbe1ea6f0849042c621d1bf98d804f684a68
anna-braga-jesus <annajesus.nave@gmail.com>
2022-11-27 17:07:42 -0300

    Final

--- backend/database/sql.sql
+++ backend/database/sql.sql
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+CREATE USER docker;
 CREATE DATABASE carrefour;
 CREATE TABLE mensagens (
 id SERIAL PRIMARY KEY,
@@ -5,5 +10,7 @@ nome varchar(50) ,
 email varchar (50) ,
 comentario varchar(100)
 );
--- CREATE TABLE sessions(id SERIAL PRIMARY KEY, email varchar(50), );
 INSERT INTO mensagens (id, nome, email, comentario) VALUES (1, 'Anna', 'annajesus.nave@gmail.com', 'Tentativa');
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+EOSQL
